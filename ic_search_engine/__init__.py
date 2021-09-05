d8531d92968c40eb89b2a235807383edf1c8d4d6
Mallika Sirdeshpande <sird.mallika@gmail.com>
2021-09-05 20:09:31 +0530

    Changed to accomodate for React Frontend

--- ic_search_engine/__init__.py
+++ ic_search_engine/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from .main.routes import main
 from .extensions import mongo
 from dotenv import load_dotenv
+from flask_cors import CORS
 
 def create_app(config_object='ic_search_engine.settings'):
     load_dotenv()
-    app = Flask(__name__)
+    app = Flask(__name__,static_folder='frontend\\build\\',static_url_path='')
+    CORS(app)
     app.config.from_object(config_object)
     #app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
     mongo.init_app(app)
--- ic_search_engine/main/routes.py
+++ ic_search_engine/main/routes.py
@@ -3,7 +3,7 @@
 '''
 
 #Importing required files
-from flask import Blueprint, render_template, redirect, url_for, request, jsonify, current_app
+from flask import Blueprint, send_from_directory, redirect, url_for, request, jsonify, current_app
 from werkzeug.utils import secure_filename
 from ic_search_engine.extensions import mongo
 from .models import integrated_circuit
@@ -30,7 +30,7 @@ def allowed_file(filename):
 @main.route('/', methods=['GET'])
 def index():
     #Renders the template in index.html
-    return render_template('index.html')
+    return send_from_directory(current_app.static_folder,'index.html')
 
 '''
     Route to query for ICs.
@@ -54,47 +54,53 @@ def query_data():
 @main.route('/add',methods=['POST','GET'])
 #TODO: Add some form of authentication to be able to access this resource.
 def add_data():
-    # check if the post request has the file part
-    if request.method == 'POST':
-        #Check if the incoming request contains a file
-        print(dir(request))
-        print(request.files)
-        if 'pinout_diagram' not in request.files:
-            return {'error':'Please upload the required file'}
-        pinout_diagram = request.files['pinout_diagram']
-        #In case filename is empty (file hasn't been uploaded)
-        if pinout_diagram.filename == '':
-            return {'error':'Please select a pinout file'}
-        #Check if the uploaded pinout diagram is a valid image file
-        if pinout_diagram and allowed_file(pinout_diagram.filename):
-            #Randomly generated file name for security reasons
-            filename = secure_filename(pinout_diagram.filename)
-            #Save the file to a temporary location while we retrieve pinout from it.
-            pinout_diagram.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-            #Get data from pinout. If error, send back error msg.
-            #STEP 0: Get the path of the file just saved
-            file_path = os.path.join(current_app.config['UPLOAD_FOLDER'],filename)
-            #Putting steps 1 and 2 in a try and except block to handle errors.
-            try:
-                #STEP 1: Create an identifier object. (The class exists in ocr.py)
-                ocr_object = identifier(file_path)
-                #STEP 2: Get pinout information
-                pinout_info = ocr_object.get_pins_dict()
-            except:
+    try:
+        # check if the post request has the file part
+        if request.method == 'POST':
+            #Check if the incoming request contains a file
+            print(request.files)
+            for i in request.files:
+                print(i)
+            print(request.json)
+            if 'pinout_diagram' not in request.files:
+                return {'error':'Please upload the required file'}, 406
+            pinout_diagram = request.files['pinout_diagram']
+            #In case filename is empty (file hasn't been uploaded)
+            if pinout_diagram.filename == '':
+                return {'error':'Please select a pinout file'}, 406
+            #Check if the uploaded pinout diagram is a valid image file
+            if pinout_diagram and allowed_file(pinout_diagram.filename):
+                #Randomly generated file name for security reasons
+                filename = secure_filename(pinout_diagram.filename)
+                #Save the file to a temporary location while we retrieve pinout from it.
+                pinout_diagram.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+                #Get data from pinout. If error, send back error msg.
+                #STEP 0: Get the path of the file just saved
+                file_path = os.path.join(current_app.config['UPLOAD_FOLDER'],filename)
+                #Putting steps 1 and 2 in a try and except block to handle errors.
+                try:
+                    #STEP 1: Create an identifier object. (The class exists in ocr.py)
+                    ocr_object = identifier(file_path)
+                    #STEP 2: Get pinout information
+                    pinout_info = ocr_object.get_pins_dict()
+                except Exception as e:
+                    #delete saved image, then return error message
+                    #os.system('rm '+file_path)
+                    return {'error':str(e)}, 418
+                    #return {'error':'There was some error while reading pinout information from the image. Try again later.'}, 418
+                #If all goes well:
+                ic_collection = mongo.db.ic_data
+                ic = integrated_circuit(request.json)
+                ic_collection.insert_one(ic.get_data('dict'))
+                #delete saved image, then send success message
+                #os.system('rm '+file_path)
+                return ic.get_data('json')
+            #If pinout image is of the wrong format
+            else:
                 #delete saved image, then returh error message
-                os.system('rm '+file_path)
-                return {'error':'There was some error while reading pinout information from the image. Try again later.'}
-            #If all goes well:
-            ic_collection = mongo.db.ic_data
-            ic = integrated_circuit(request.json)
-            ic_collection.insert_one(ic.get_data('dict'))
-            #delete saved image, then send success message
-            os.system('rm '+file_path)
-            return ic.get_data('json')
-        #If pinout image is of the wrong format
-        else:
-            #delete saved image, then returh error message
-            os.system('rm '+file_path)
-            return {'error':'Please select a valid pinout file'}
-    elif request.method == 'GET':
-        return render_template('add.html')
+                #os.system('rm '+file_path)
+                return {'error':'Please select a valid pinout file'}, 415
+        elif request.method == 'GET':
+            return send_from_directory(current_app.static_folder,'add.html')
+    except:
+        return {'error':'Something bad on our end, sorry!'}, 500
